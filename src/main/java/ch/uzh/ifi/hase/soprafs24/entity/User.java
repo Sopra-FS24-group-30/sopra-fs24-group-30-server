e1f866925fb56807483192b2b0e1bd53ef8f6f0a
celineziegler <you@example.com>
2024-03-31 23:30:04 +0200

    Deleted value: "name" and all the additional tests

--- src/main/java/ch/uzh/ifi/hase/soprafs24/entity/User.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/entity/User.java
@@ -33,9 +33,6 @@ public class User implements Serializable {
     private Long id;
 
     @Column(nullable = false, unique = true)
-    private String name;
-
-    @Column(nullable = false, unique = true)
     private String username;
 
     @Column(nullable = false, unique = true)
@@ -59,14 +56,6 @@ public class User implements Serializable {
 
     public void setId(Long id) {
         this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getUsername() {
