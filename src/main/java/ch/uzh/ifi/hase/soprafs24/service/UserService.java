9014848f9d4236f09cdacfa88a109b0261ca698f
Ambros Eberhard <ambros.eberhard@gmx.ch>
2024-04-10 21:28:26 +0200

    #215 added creating of 6 digit code for lobby

--- src/main/java/ch/uzh/ifi/hase/soprafs24/service/UserService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/service/UserService.java
@@ -14,8 +14,10 @@ import org.springframework.web.server.ResponseStatusException;
 
 import javax.transaction.Transactional;
 import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 @Transactional
@@ -67,6 +69,16 @@ public class UserService {
         return foundUser.get();
     }
 
+    public String getLobbyId(){
+        StringBuilder lobbyId = new StringBuilder();
+        for(int i=0; i<6;i++){
+            lobbyId.append(Integer.toString(ThreadLocalRandom.current().nextInt(0, 10)));
+        }
+
+        //TODO check against already active lobbies to avoid conflicts
+        return lobbyId.toString();
+    }
+
 
     private boolean checkUsernameExists(String username){
         Optional<User> existingUser = this.UserRepository.findByUsername(username);
@@ -75,4 +87,6 @@ public class UserService {
         }
         return false;
     }
+
+
 }
