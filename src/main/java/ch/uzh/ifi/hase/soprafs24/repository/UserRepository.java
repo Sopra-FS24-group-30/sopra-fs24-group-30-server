26768e7b09087e53353b63e75d894ef30b6c941b
ambroseberhard <ambros.eberhard@gmx.ch>
2024-04-10 19:51:13 +0200

    changed code to use optional type for easier access in other module

--- src/main/java/ch/uzh/ifi/hase/soprafs24/repository/UserRepository.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/repository/UserRepository.java
@@ -4,7 +4,9 @@ import ch.uzh.ifi.hase.soprafs24.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository("UserRepository")
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username);
 }
