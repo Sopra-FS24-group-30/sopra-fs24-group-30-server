2aac9d1cd3765da2442b1bc72795dcc3b3d49841
ambroseberhard <ambros.eberhard@gmx.ch>
2024-04-11 20:44:19 +0200

    added player idea

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/logic/Game/Player.java
@@ -0,0 +1,16 @@
+package ch.uzh.ifi.hase.soprafs24.logic.Game;
+
+
+public class Player {
+
+    private int id;
+    private String name;
+    private int cash;
+    //TODO verify how position is saved for player in backend (id of space or space itself)
+    private int position;
+    //TODO import card, item and ultimate
+    //ArrayList<Card> cards;
+    //ArrayList<Item> items;
+    //private Ultimate ultimate;
+    private Player teammateId;
+}
