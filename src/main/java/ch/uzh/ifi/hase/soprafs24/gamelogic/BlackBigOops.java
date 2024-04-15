812740f64d42ddb73bab3daa5dbfdea240f0d9c7
Thi Tam Gian Nguyen <thitamgian.nguyen@uzh.ch>
2024-04-15 15:57:55 +0200

    spaces, and their classes

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/gamelogic/BlackBigOops.java
@@ -0,0 +1,5 @@
+package ch.uzh.ifi.hase.soprafs24.gamelogic;
+
+public class BlackBigOops {
+    private static final String[] blacks = {"swapPosiOfAllPlayers", "lose69Cash", "evenlyDistributeAllCash"};
+}
---
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/gamelogic/RedSmallOops.java
@@ -0,0 +1,5 @@
+package ch.uzh.ifi.hase.soprafs24.gamelogic;
+
+public class RedSmallOops {
+    private static final String[] reds = {"youLose10Cash", "everyoneLose10Cash", "tpToStart"};
+}
