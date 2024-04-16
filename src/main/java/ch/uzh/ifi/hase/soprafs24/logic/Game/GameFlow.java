0aa0859041c1552d5988462648986017c8986875
Thi Tam Gian Nguyen <thitamgian.nguyen@uzh.ch>
2024-04-16 23:30:09 +0200

    linear movement for player, needs movement for junctions

--- src/main/java/ch/uzh/ifi/hase/soprafs24/logic/Game/GameFlow.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/logic/Game/GameFlow.java
@@ -1,6 +1,9 @@
 package ch.uzh.ifi.hase.soprafs24.logic.Game;
 
-import java.util.Random;
+import ch.uzh.ifi.hase.soprafs24.entity.GameBoard;
+import ch.uzh.ifi.hase.soprafs24.entity.GameBoardSpace;
+
+import java.util.List;
 
 public class GameFlow {
 
@@ -15,19 +18,55 @@ public class GameFlow {
     private void useItem(){}
     private void useCard(){}
 
-    private int throwDice(){
-        Random random = new Random();
-        int randomNumber = random.nextInt(6) + 1;
-        return randomNumber;
+    public int throwDice(){
+        return (int) Math.floor(Math.random() * 6 + 1);
     }
 
     private void updatePlayer(){}
 
-    private void updatePlayerPosition(){
-        int diceThrow = throwDice();
-        while (diceThrow > 0){
-            //diceThrow--;
+    public void updatePlayerPosition(GameBoard gameBoard, Player player, int moves){
+        Long currPosi = player.getPosition();
+        //currPosi = 42L;
+        List<GameBoardSpace> spacies = gameBoard.getSpaces();
+        int total = spacies.size();
+        while (moves > 0) {
+            List<String> nexi = null;
+            String nextSpaceColor = null;
+            //TODO: replace for loop with smth to find space by Id directly from list
+            for (int i = 0; i < total; i++) {
+                GameBoardSpace unoSpace = spacies.get(i);
+                if (unoSpace.getSpaceId().equals(currPosi)) {
+                    nexi = unoSpace.getNext();
+
+                    // get Color of next space, look if next space is a junction
+                    int juncti = Integer.parseInt(nexi.get(0));
+                    nextSpaceColor = spacies.get(juncti-1).getColor();
+
+                    break;
+                }
+            }
+
+            if (nexi.size() == 1 && !nextSpaceColor.equals("Junction") && !nextSpaceColor.equals("Gate") && !nextSpaceColor.equals("SpecialItem")){
+                Long newposi = Long.parseLong(nexi.get(0));
+                player.setPosition(newposi);
+                currPosi = newposi;
+                moves--;
+            }
+            else if (nexi.size() == 1){
+                Long newposi = Long.parseLong(nexi.get(0));
+                player.setPosition(newposi);
+                currPosi = newposi;
+            }
+            else {
+                //TODO: junctions how to
+                //currently taking the first next of list
+                Long newposi = Long.parseLong(nexi.get(0));
+                player.setPosition(newposi);
+                currPosi = newposi;
+                moves--;
+            }
         }
+        System.out.println(String.format("effect for space %d", currPosi));
     }
 
     private void turn(){}
