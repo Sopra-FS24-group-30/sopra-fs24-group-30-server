1459c7fb04a96e2e7fc738722cc3c4d4292c1f14
Thi Tam Gian Nguyen <thitamgian.nguyen@uzh.ch>
2024-04-19 14:05:23 +0200

    Comments for what the spaces do

--- src/main/java/ch/uzh/ifi/hase/soprafs24/logic/Game/CatNami.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/logic/Game/CatNami.java
@@ -1,8 +1,8 @@
 package ch.uzh.ifi.hase.soprafs24.logic.Game;
 
-import java.util.HashMap;
-import java.util.Map;
-
+/**
+ * CatNami space effects
+ */
 public class CatNami {
     private static final String[] catNamis = {"exchangeWinCondiWTeammate", "exchangeWinCondiWEnemy", "gain69Money"};
 }
--- src/main/java/ch/uzh/ifi/hase/soprafs24/logic/Game/RedSmallOops.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/logic/Game/RedSmallOops.java
@@ -1,5 +1,8 @@
 package ch.uzh.ifi.hase.soprafs24.logic.Game;
 
+/**
+ * Red spaces effects
+ */
 public class RedSmallOops {
     private static final String[] reds = {"youLose10Cash", "everyoneLose10Cash", "tpToStart"};
 }
