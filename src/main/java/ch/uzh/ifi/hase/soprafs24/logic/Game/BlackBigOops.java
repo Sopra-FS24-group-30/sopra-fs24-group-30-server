30eab209c24c08b128bd9d84f0aac3b1133b06f7
Thi Tam Gian Nguyen <thitamgian.nguyen@uzh.ch>
2024-04-19 14:05:23 +0200

    Comments for what the spaces do

--- src/main/java/ch/uzh/ifi/hase/soprafs24/logic/Game/BlackBigOops.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/logic/Game/BlackBigOops.java
@@ -1,5 +1,8 @@
 package ch.uzh.ifi.hase.soprafs24.logic.Game;
 
+/**
+ * Black space effects
+ */
 public class BlackBigOops {
     private static final String[] blacks = {"swapPosiOfAllPlayers", "lose69Cash", "evenlyDistributeAllCash"};
 }
--- src/main/java/ch/uzh/ifi/hase/soprafs24/logic/Game/Item.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/logic/Game/Item.java
@@ -1,8 +1,14 @@
 package ch.uzh.ifi.hase.soprafs24.logic.Game;
 
-import java.util.HashMap;
-import java.util.Map;
-
+/**
+ * Items and what they do:
+ * - MagicMushroom: This turn you roll 2 dice. If you roll doubles: +10 Cash.
+ * - SuperMagicMushroom: This turn you roll 3 dice. If you roll triplets: +30 Cash
+ * - UltraMagicMushroom: This turn you roll 4 dice. If you roll quadruplets: +69 Cash.
+ * - OnlyFansAbo: Steal 7 coins from every other player (even your Teammate (simp))
+ * - TreasureChest: Steal a random Item from a player of your choice.
+ * - TheBrotherAndCo: Use this item to pass a gate, you can use this item at a gate, even if you have already used an item this turn.
+ */
 public class Item {
 
     private static final String[] goldItems = {"UltraMagicMushroom", "OnlyFansAbo"};
