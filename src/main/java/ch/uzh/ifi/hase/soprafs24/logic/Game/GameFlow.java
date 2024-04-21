95f5a0d43f1b4e1e68211c892ceba0e517bc5f37
Thi Tam Gian Nguyen <thitamgian.nguyen@uzh.ch>
2024-04-21 19:54:53 +0200

    rebase

--- src/main/java/ch/uzh/ifi/hase/soprafs24/logic/Game/GameFlow.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/logic/Game/GameFlow.java
@@ -25,43 +25,101 @@ public class GameFlow {
         return (int) (Math.random() * 6 + 1);
     }
 
-    private void updatePlayer(){} //NOSONAR
+    //maybe split update player up into cash,item,card,posi?
+    private void updatePlayer(){}
 
-    public void updatePlayerPosition(GameBoard gameBoard, Player player, int moves) {
-        Long currPosi = player.getPosition();
-        //currPosi = 15L; //dummyPosi
-        List<GameBoardSpace> allSpaces = gameBoard.getSpaces();
-        List<Long> listi = new ArrayList<>();
-        String color = null;
+    //normal walk
+    public Map<String, Object> updatePlayerPosition(GameBoard gameBoard, Player player, int moves, long posi) {
+        //Long currPosi = player.getPosition();
+        Long currPosi = posi;
         int movies = moves;
-        while (moves > 0) {
-            GameBoardSpace currentSpace = findSpaceById(allSpaces, currPosi);
+        List<GameBoardSpace> allSpaces = gameBoard.getSpaces(); //list of all spaces
 
-            List<String> nextSpaceIds = currentSpace.getNext();
+        List<Long> listi = new ArrayList<>(); //list of spaceIds that player moves over
+        String color = null; //color of next space
 
+        GameBoardSpace currentSpace = new GameBoardSpace(); //space currently on
+        List<String> nextSpaceIds; //list of next spaces of space currently on
+
+        while (movies > 0) {
+            currentSpace = findSpaceById(allSpaces, currPosi);
+            nextSpaceIds = currentSpace.getNext();
+
+            // if next space is linear (not junction/gate)
             if (nextSpaceIds.size() == 1) {
-                String nextSpaceId = nextSpaceIds.get(0);
-                Long nextPosi = Long.parseLong(nextSpaceId);
+                Long nextPosi = Long.parseLong(nextSpaceIds.get(0));
                 listi.add(nextPosi);
-                player.setPosition(nextPosi);
-                currPosi = nextPosi;
-                GameBoardSpace nextSpace = findSpaceById(allSpaces, nextPosi);
+
+                GameBoardSpace nextSpace = findSpaceById(allSpaces, nextPosi); // space next on
                 color = nextSpace.getColor();
 
-                if (nextSpace.getOnSpace() == null) {
-                    break;
+                //set player to next posi already
+                currPosi = nextPosi;
+                player.setPosition(currPosi);
 
+                // check if game is over, or player gets cash
+                if ("BlueGoal".equals(color) && nextSpace.getIsGoal()) {
+                    if (player.getCanWin()) {
+                        // TODO GAME OVER
+                        return toReturn(player, listi, moves, color);
+                    }
+                    player.setCash(player.getCash() + 15);
+                }
+
+                // "partial end of walk", check on what decision can be done.
+                // walk can never end in these space types btw (junction, gate, specialItem)
+                if (nextSpace.getOnSpace() == null) {
+                    if (color.equals("Junction")){
+                        // return the currently walked spaces, after getting a response, continue with the walk
+                        //TODO: BUT HOW TO HANDLE RESPONSE OF JUNCTION
+                        // like handled in SpaceEffects
+                        return toReturn(player, listi, moves, color);
+                    }
+                    else if (color.equals("Gate")){
+                        //check if brother exists
+                        // - if yes then return the currently walked spaces (like junction)
+                        //TODO: BUT HOW TO HANDLE RESPONSE OF GATE
+                        // like handled in SpaceEffects
+                        for (Item item : player.getItems()){
+                            if (item.getItemName().equals("TheBrotherAndCo")){
+                                System.out.println("nono");
+                                return toReturn(player,listi,moves,color);
+                            }
+                        }
+                        // - if no then not even ask, just continue
+                        // prob handle with recursion instead of just continuing
+                        // TODO ugly ass Gate handling
+                        currentSpace = findSpaceById(allSpaces, currPosi);
+                        nextSpaceIds = currentSpace.getNext();
+                        nextPosi = Long.parseLong(nextSpaceIds.get(0));
+                        listi.add(nextPosi);
+                        nextSpace = findSpaceById(allSpaces, nextPosi);
+                        color = nextSpace.getColor();
+                        currPosi = nextPosi;
+                        player.setPosition(currPosi);
+                    }
+                    else if (color.equals("SpecialItem")){
+                        //add an item to the player item list
+                        SpaceEffects.specialItem(player);
+                        return toReturn(player,listi,moves,color);
+                    }
                     //send data to friend (via websocket)
                     //recursively call this functiön
                 }
-                moves--;
             }
+            movies--;
         }
-        Map<String, Object> dici = new HashMap<>();
-        dici.put("spaces", listi);
-        dici.put("moves", movies);
-        dici.put("spaceColor", color);
-        // send dici to frontend
+
+        //at the end of the walk
+        if ("Yellow".equals(color)){
+            player.setLandYellow(player.getLandYellow()+1);
+        }
+        else if ("CatNami".equals(color)){
+            player.setLandCat(player.getLandCat()+1);
+        }
+        //TODO THE SPACE EFFECT NOW/LATER??
+        SpaceEffects.getSpaceEffectValue(currentSpace.getOnSpace());
+        return toReturn(player, listi, moves, color);
     }
 
     //helper for finding Space by Id
@@ -74,7 +132,18 @@ public class GameFlow {
         return null;
     }
 
-    private void turn(){} //NOSONAR
+    //helper for data representation as dict
+    private Map<String, Object> toReturn(Player player, List<Long> walkedSpaces, int initialMoves, String landedSpace){
+        Map<String, Object> data = new HashMap<>();
+        data.put("spaces", walkedSpaces);
+        data.put("moves", initialMoves);
+        data.put("spaceColor", landedSpace);
+        Map<String, Object> retour = new HashMap<>();
+        retour.put(player.getPlayerId().toString(), data);
+        return retour;
+    }
+
+    private void turn(){}
 
 
     private void run(){} //NOSONAR
--- src/main/java/ch/uzh/ifi/hase/soprafs24/logic/Game/Item.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/logic/Game/Item.java
@@ -1,6 +1,10 @@
 package ch.uzh.ifi.hase.soprafs24.logic.Game;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
+ * One can acquire and use items during the game.
  * Items and what they do:
  * - MagicMushroom: This turn you roll 2 dice. If you roll doubles: +10 Cash.
  * - SuperMagicMushroom: This turn you roll 3 dice. If you roll triplets: +30 Cash
@@ -10,8 +14,28 @@ package ch.uzh.ifi.hase.soprafs24.logic.Game;
  * - TheBrotherAndCo: Use this item to pass a gate, you can use this item at a gate, even if you have already used an item this turn.
  */
 public class Item {
+    private String itemName;
 
-    private static final String[] goldItems = {"UltraMagicMushroom", "OnlyFansAbo"};
-    private static final String[] silverItems = {"SuperMagicMushroom", "TreasureChest"};
-    private static final String[] bronzeItems = {"MagicMushroom", "TheBrotherAndCo"};
+    public Item(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public static List<Item> getAllItems() {
+        List<Item> allItems = new ArrayList<>();
+        allItems.add(new Item("TheBrotherAndCo"));
+        allItems.add(new Item("MagicMushroom"));
+        allItems.add(new Item("SuperMagicMushroom"));
+        allItems.add(new Item("UltraMagicMushroom"));
+        allItems.add(new Item("OnlyFansAbo"));
+        allItems.add(new Item("TreasureChest"));
+        return allItems;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+    //Ambrosses art von class und three subclass how
+    //class MushroomItem extends Item {//MagicMushroom, SuperMagicMushroom, UltraMagicMushroom}
+    //class BrothaItem extends Item {//TheBrotherAndCo}
+    //class OtherItem extends Item {//TreasureChest, OnlyFansAbo}
 }
