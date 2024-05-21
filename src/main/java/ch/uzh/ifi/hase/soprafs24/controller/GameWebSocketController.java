3a88a9524261b7543304edbfb3f55a1e91deafb4
ambroseberhard <ambros.eberhard@gmx.ch>
2024-05-21 13:22:13 +0200

    changed getElapsedTime to return time in miliseconds instead of seconds

--- src/main/java/ch/uzh/ifi/hase/soprafs24/controller/GameWebSocketController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/controller/GameWebSocketController.java
@@ -79,7 +79,7 @@ public class GameWebSocketController {
         }
 
         public long getElapsedTime() {
-            return elapsedTime / 1000; // Return elapsed time in seconds
+            return elapsedTime; // Return elapsed time in seconds
         }
 
         public void setElapsedTime(long elapsedTime) {
@@ -88,7 +88,7 @@ public class GameWebSocketController {
 
         // Check if the elapsed time exceeds the maximum time
         public boolean maxTimeReached(long maxTimeInSeconds) {
-            return getElapsedTime() >= maxTimeInSeconds;
+            return (getElapsedTime()/1000) >= maxTimeInSeconds;
         }
 
         public void stopTimer() {
