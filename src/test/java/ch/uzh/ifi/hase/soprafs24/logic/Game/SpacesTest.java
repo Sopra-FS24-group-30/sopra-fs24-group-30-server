c85e2928ad91d18251cc7c75ad750aa94148aeb0
Ambros Eberhard <ambros.eberhard@gmx.ch>
2024-05-22 08:46:08 +0200

    added last tests

--- src/test/java/ch/uzh/ifi/hase/soprafs24/logic/Game/SpacesTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs24/logic/Game/SpacesTest.java
@@ -2,7 +2,9 @@ package ch.uzh.ifi.hase.soprafs24.logic.Game;
 
 import ch.uzh.ifi.hase.soprafs24.controller.GameWebSocketController;
 import ch.uzh.ifi.hase.soprafs24.logic.Game.Constant.GamblingChoice;
+import org.hibernate.query.criteria.internal.expression.SimpleCaseExpression;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -500,5 +502,128 @@ public class SpacesTest {
         assertEquals(2,gameFlow.getPlayer(1).getItemNames().size());
     }
 
+    @Test
+    void testGift10MoneyPlayCanPay(){
+        GameFlow gameFlow = basicGameFlowSetup();
 
+        spaces.gift10Money(gameFlow);
+
+        assertEquals(90,gameFlow.getPlayer(1).getCash());
+        assertEquals(110,gameFlow.getPlayer(3).getCash());
+    }
+
+    @Test
+    void testGift10MoneyPlayCantPay(){
+        GameFlow gameFlow = basicGameFlowSetup();
+        gameFlow.getPlayer(1).setCash(7);
+
+        spaces.gift10Money(gameFlow);
+
+        assertEquals(0,gameFlow.getPlayer(1).getCash());
+        assertEquals(107,gameFlow.getPlayer(3).getCash());
+    }
+
+    @Test
+    void testSellAllCardsOneOfEach(){
+        GameFlow gameFlow = basicGameFlowSetup();
+        gameFlow.getPlayer(1).addCardNames("B26");
+        gameFlow.getPlayer(1).addCardNames("S0");
+        gameFlow.getPlayer(1).addCardNames("G13");
+
+        spaces.sellAllCards(gameFlow);
+
+        //sold all cards for  total of 5 + 5 + 7 + 10 = 27
+        assertEquals(127,gameFlow.getPlayer(1).getCash());
+        assertEquals(0,gameFlow.getPlayer(1).getCardNames().size());
+    }
+
+    @Test
+    void testGetOtherCardsAllHaveCard(){
+        GameFlow gameFlow = basicGameFlowSetup();
+
+        spaces.getOthersCards(gameFlow);
+
+        assertEquals(4,gameFlow.getPlayer(1).getCardNames().size());
+        assertEquals(0,gameFlow.getPlayer(2).getCardNames().size());
+        for(String card : gameFlow.getPlayer(1).getCardNames()){
+            assertEquals("B26",card);
+        }
+    }
+
+    @Test
+    void testGetOtherCardsSomeHaveNoCard(){
+        GameFlow gameFlow = basicGameFlowSetup();
+        gameFlow.getPlayer(2).removeCardNames("B26");
+
+        spaces.getOthersCards(gameFlow);
+
+        assertEquals(3,gameFlow.getPlayer(1).getCardNames().size());
+        assertEquals(100,gameFlow.getPlayer(1).getCash());
+        assertEquals(95,gameFlow.getPlayer(2).getCash());
+    }
+
+    @Test
+    void testGetOtherCardsSomeHaveNoCardAndNotEnoughMoney(){
+        GameFlow gameFlow = basicGameFlowSetup();
+        gameFlow.getPlayer(2).removeCardNames("B26");
+        gameFlow.getPlayer(2).setCash(3);
+        gameFlow.getPlayer(3).removeCardNames("B26");
+        gameFlow.getPlayer(3).setCash(0);
+
+        spaces.getOthersCards(gameFlow);
+
+        assertEquals(2,gameFlow.getPlayer(1).getCardNames().size());
+        assertEquals(100,gameFlow.getPlayer(1).getCash());
+        assertEquals(0,gameFlow.getPlayer(2).getCash());
+        assertEquals(0,gameFlow.getPlayer(3).getCash());
+    }
+
+    @Test
+    void testSurpriseMFTriggersCatNami(){
+        GameFlow gameFlow = basicGameFlowSetup();
+
+        ArgumentCaptor<GameFlow> argumentCaptor = ArgumentCaptor.forClass(GameFlow.class);
+        spaces = Mockito.spy(spaces);
+        when(spaces.randomInt(4)).thenReturn(0);
+
+        spaces.surpriseMF(gameFlow);
+
+        verify(spaces).catnami(argumentCaptor.capture());
+        assertEquals(gameFlow,argumentCaptor.getValue());
+    }
+
+    @Test
+    void testSwapCardsOrItemsItemSwapOddTeam(){
+        GameFlow gameFlow = basicGameFlowSetup();
+        gameFlow.getPlayer(1).removeItemNames("OnlyFansAbo");
+        gameFlow.getPlayer(1).addItemNames("MagicMushroom");
+        spaces = Mockito.spy(spaces);
+        when(spaces.randomInt(2)).thenReturn(0);
+
+        spaces.swapCardsOrItems(gameFlow);
+
+        assertEquals(1,gameFlow.getPlayer(1).getItemNames().size());
+        assertEquals("OnlyFansAbo",gameFlow.getPlayer(1).getItemNames().get(0));
+        assertEquals(1,gameFlow.getPlayer(3).getItemNames().size());
+        assertEquals("MagicMushroom",gameFlow.getPlayer(3).getItemNames().get(0));
+    }
+
+    @Test
+    void testSwapCardsOrItemsCardSwapEvenTeam(){
+        GameFlow gameFlow = basicGameFlowSetup();
+        gameFlow.getPlayer(2).removeCardNames("B26");
+        gameFlow.getPlayer(2).addCardNames("S0");
+        gameFlow.getPlayer(2).addCardNames("G13");
+        gameFlow.setTurnPlayerId(2L);
+        spaces = Mockito.spy(spaces);
+        when(spaces.randomInt(2)).thenReturn(1);
+
+        spaces.swapCardsOrItems(gameFlow);
+
+        assertEquals(2,gameFlow.getPlayer(4).getCardNames().size());
+        assertEquals("S0",gameFlow.getPlayer(4).getCardNames().get(0));
+        assertEquals("G13",gameFlow.getPlayer(4).getCardNames().get(1));
+        assertEquals(1,gameFlow.getPlayer(2).getCardNames().size());
+        assertEquals("B26",gameFlow.getPlayer(2).getCardNames().get(0));
+    }
 }
