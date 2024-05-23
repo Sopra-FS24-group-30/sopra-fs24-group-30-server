06f5c8c0e55992be13d0dbeaaf938e974aa6b10a
Carlos Hernandez <carlosandres.hernandezpadilla@uzh.ch>
2024-05-24 01:52:09 +0200

    tests for gameflow

--- src/test/java/ch/uzh/ifi/hase/soprafs24/service/AchievementServiceTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs24/service/AchievementServiceTest.java
@@ -3,6 +3,8 @@ package ch.uzh.ifi.hase.soprafs24.service;
 import ch.uzh.ifi.hase.soprafs24.controller.GameWebSocketController;
 import ch.uzh.ifi.hase.soprafs24.entity.AchievementStatus;
 import ch.uzh.ifi.hase.soprafs24.entity.User;
+import ch.uzh.ifi.hase.soprafs24.entity.Game;
+import ch.uzh.ifi.hase.soprafs24.entity.GameBoard;
 import ch.uzh.ifi.hase.soprafs24.logic.Game.AchievementProgress;
 import ch.uzh.ifi.hase.soprafs24.logic.Game.GameFlow;
 import ch.uzh.ifi.hase.soprafs24.logic.Game.Player;
@@ -15,6 +17,7 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import ch.uzh.ifi.hase.soprafs24.logic.Game.Effects.Getem;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
@@ -23,6 +26,7 @@ import java.util.Set;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.*;
+import org.json.JSONObject;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -197,6 +201,104 @@ public class AchievementServiceTest {
         assertEquals(0,saved.getTotalGamesWon());
     }
 
+    @Test
+    public void checkGameOverWinCondi() {
+        GameFlow gameFlow = basicGameFlowSetup();
+        gameFlow.getPlayer(1).setTeammateId(3L);
+        gameFlow.getPlayer(2).setTeammateId(4L);
+        gameFlow.getPlayer(3).setTeammateId(1L);
+        gameFlow.getPlayer(4).setTeammateId(2L);
+        Player[] players = gameFlow.getPlayers();
+        for (Player p : players) {
+            p.setWinCondition("Unlucky");
+        }
+        Set<String> winners = new HashSet<>();
+        Map<String, Object> mappi = new HashMap<>();
+        List<Long> rich = new ArrayList<>();
+        User user = simplestUser();
+        achievementService.saveInitialAchievements(user);
+        User user1 = simplestUser();
+        achievementService.saveInitialAchievements(user1);
+        User user2 = simplestUser();
+        achievementService.saveInitialAchievements(user2);
+        User user3 = simplestUser();
+        achievementService.saveInitialAchievements(user3);
+        players[0].setUser(user);
+        players[1].setUser(user1);
+        players[2].setUser(user2);
+        players[3].setUser(user3);
+        rich.add(1L);
+        players[0].setLostCash(40);
+        Map<String, Object> map = new HashMap<>();
+        map.put("winners", null);
+        map.put("reason", "1 has Unlucky");
+        AchievementStatus saved = achievementRepository.findByUserId(1L);
+        assertEquals(map.toString(), gameFlow.doGameOverWinCondi(players[0]).toString());
+    }
+
+    @Test
+    public void checkGameOverWinCondi2() {
+        GameFlow gameFlow = basicGameFlowSetup();
+        gameFlow.getPlayer(1).setTeammateId(3L);
+        gameFlow.getPlayer(2).setTeammateId(4L);
+        gameFlow.getPlayer(3).setTeammateId(1L);
+        gameFlow.getPlayer(4).setTeammateId(2L);
+        Player[] players = gameFlow.getPlayers();
+        for (Player p : players) {
+            p.setWinCondition("Marooned");
+        }
+        Set<String> winners = new HashSet<>();
+        Map<String, Object> mappi = new HashMap<>();
+        List<Long> rich = new ArrayList<>();
+        User user = simplestUser();
+        achievementService.saveInitialAchievements(user);
+        User user1 = simplestUser();
+        achievementService.saveInitialAchievements(user1);
+        User user2 = simplestUser();
+        achievementService.saveInitialAchievements(user2);
+        User user3 = simplestUser();
+        achievementService.saveInitialAchievements(user3);
+        players[0].setUser(user);
+        players[1].setUser(user1);
+        players[2].setUser(user2);
+        players[3].setUser(user3);
+        rich.add(1L);
+        players[0].setLostCash(40);
+        Map<String, Object> map = new HashMap<>();
+        map.put("winners", null);
+        map.put("reason", "1 has Marooned");
+        AchievementStatus saved = achievementRepository.findByUserId(1L);
+        assertEquals(map.toString(), gameFlow.doGameOverWinCondi(players[0]).toString());
+    }
+
+    @Test
+    public void TestCheckWinCondition() {
+        GameFlow gameFlow = basicGameFlowSetup();
+        Player[] players = gameFlow.getPlayers();
+        for (Player p : players) {
+            p.setWinCondition("Golden");
+        }
+        gameFlow.checkWinCondition(players[0]);
+    }
+
+    @Test
+    public void TestCheckWinCondition1() {
+        GameFlow gameFlow = basicGameFlowSetup();
+        Player[] players = gameFlow.getPlayers();
+        for (Player p : players) {
+            p.setWinCondition("Marooned");
+        }
+        gameFlow.checkWinCondition(players[0]);
+    }
+    @Test
+    public void TestCheckWinCondition2() {
+        GameFlow gameFlow = basicGameFlowSetup();
+        Player[] players = gameFlow.getPlayers();
+        for (Player p : players) {
+            p.setWinCondition("Drunk");
+        }
+        gameFlow.checkWinCondition(players[0]);
+    }
 
     @Test
     public void testCorrectUpdateWinLoseStreak(){
@@ -425,5 +527,110 @@ public class AchievementServiceTest {
         assertTrue(savedBackstabber.isBackStabber());
     }
 
+    @Test
+    public void TestCheckWinCondition3() {
+        User user = simplestUser();
+        achievementService.saveInitialAchievements(user);
+        AchievementStatus saved = achievementRepository.findByUserId(1L);
 
+        GameFlow gameFlow = basicGameFlowSetup();
+        Player[] players = gameFlow.getPlayers();
+        for (Player p : players) {
+            p.setWinCondition("Ship");
+        }
+        players[0].setUser(user);
+        players[0].setUserId(1L);
+        gameFlow.checkWinCondition(players[0]);
+    }
+
+    @Test
+    public void TestCheckWinCondition4() {
+        User user = simplestUser();
+        achievementService.saveInitialAchievements(user);
+        AchievementStatus saved = achievementRepository.findByUserId(1L);
+
+        GameFlow gameFlow = basicGameFlowSetup();
+        Player[] players = gameFlow.getPlayers();
+        for (Player p : players) {
+            p.setWinCondition("Explorer");
+        }
+        players[0].setUser(user);
+        players[0].setUserId(1L);
+        gameFlow.checkWinCondition(players[0]);
+    }
+
+    @Test
+    public void TestCheckWinCondition5() {
+        User user = simplestUser();
+        achievementService.saveInitialAchievements(user);
+        AchievementStatus saved = achievementRepository.findByUserId(1L);
+
+        GameFlow gameFlow = basicGameFlowSetup();
+        Player[] players = gameFlow.getPlayers();
+        for (Player p : players) {
+            p.setWinCondition("Unlucky");
+        }
+        players[0].setUser(user);
+        players[0].setUserId(1L);
+        gameFlow.checkWinCondition(players[0]);
+    }
+/*
+    @Test
+    public void TestCardPosition() {
+        GameFlow gameFlow = GameWebSocketController.getGameFlow(1L);
+        assertNotNull(gameFlow, "GameFlow should be properly initialized");
+        Player[] players = gameFlow.getPlayers();
+        players[0].setPosition(27L);
+        gameFlow.setCardDiceUsed(true);
+        gameFlow.setGameId(1L);
+        JSONObject card = Getem.getCards().get("S1");
+        gameFlow.updateCardPositions(card, -123);
+        assertEquals(28L, players[0].getPosition());
+
+    }
+*/
+
+/*
+    @Test
+    public void TestCardPosition2() {
+        basicGameFlowSetup();
+        GameFlow gameFlow1 = GameWebSocketController.getGameFlow(1L);
+        assertNotNull(gameFlow1, "GameFlow should be properly initialized");
+        Player[] players = gameFlow1.getPlayers();
+        players[0].setPosition(27L);
+        gameFlow1.setCardDiceUsed(true);
+        JSONObject card = Getem.getCards().get("G13");
+        JSONObject choices = new JSONObject();
+        choices.put("count", "1");
+        gameFlow1.setChoices(choices);
+        gameFlow1.updateCardPositions(card, gameFlow1.getChoices().getInt("count"));
+
+        assertEquals(28L, players[0].getPosition());
+    }
+/*
+    @Test
+    public void movePLayer() {
+        basicGameFlowSetup();
+        GameFlow gameFlow = GameWebSocketController.getGameFlow(1L);
+        assertNotNull(gameFlow, "GameFlow should be properly initialized");
+        Player player = gameFlow.getPlayer(1);
+        assertNotNull(player, "Player should not be null");
+        player.setPosition(27L);
+
+        long initialPosition = player.getPosition();
+        //Map<String, Object> result = gameFlow.move(2, initialPosition);
+        Map<String, Object> result = new HashMap<>();
+
+        assertEquals(result, gameFlow.move(1, initialPosition));
+    }
+*/
+/*
+    @Test
+    public void movePlayer() {
+        // Retrieve the game flow from the controller
+        GameFlow gameFlow = GameWebSocketController.getGameFlow(1L);
+        assertNotNull(gameFlow, "GameFlow should be properly initialized");
+    }
+
+*/
 }
